3a78b4fa00b0f72edd7eaa8f5ddd8fe1f02e1409
fwilbor <franklin.cfg@gmail.com>
2019-08-06 13:28:53 -0400

    add functions for OMDB and Bandsintown API

--- liri.js
+++ liri.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+
+var fs = require("fs");
+
+
+
 // var Spotify = require('node-spotify-api');
 var Spotify = require('node-spotify-api');
 
@@ -59,6 +64,81 @@ function song() {
 }
 
 song();
+
+function movieInfo(parameter) {
+
+
+    var findMovie;
+    if (parameter === undefined) {
+        findMovie = "Mr. Nobody";
+    } else {
+        findMovie = parameter;
+    };
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + findMovie + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (err, res, body) {
+        var bodyOf = JSON.parse(body);
+        if (!err && res.statusCode === 200) {
+            logIt("\n---------------------------------------------------\n");
+            logIt("Title: " + bodyOf.Title);
+            logIt("Release Year: " + bodyOf.Year);
+            logIt("IMDB Rating: " + bodyOf.imdbRating);
+            logIt("Rotten Tomatoes Rating: " + bodyOf.Ratings[1].Value);
+            logIt("Country: " + bodyOf.Country);
+            logIt("Language: " + bodyOf.Language);
+            logIt("Plot: " + bodyOf.Plot);
+            logIt("Actors: " + bodyOf.Actors);
+            logIt("\n---------------------------------------------------\n");
+        }
+    });
+};
+
+movieInfo();
+
+
+
+
+
+function getRandom() {
+    fs.readFile('random.txt', "utf8", function (error, data) {
+
+        if (error) {
+            return logIt(error);
+        }
+
+
+        var dataArr = data.split(",");
+
+        if (dataArr[0] === "spotify-this-song") {
+            var songcheck = dataArr[1].trim().slice(1, -1);
+            spotSong(songcheck);
+        }
+        else if (dataArr[0] === "concert-this") {
+            if (dataArr[1].charAt(1) === "'") {
+                var dLength = dataArr[1].length - 1;
+                var data = dataArr[1].substring(2, dLength);
+                console.log(data);
+                bandsInTown(data);
+            }
+            else {
+                var bandName = dataArr[1].trim();
+                console.log(bandName);
+                bandsInTown(bandName);
+            }
+
+        }
+        else if (dataArr[0] === "movie-this") {
+            var movie_name = dataArr[1].trim().slice(1, -1);
+            movieInfo(movie_name);
+        }
+
+    });
+
+};
+
+getRandom();
+
 
 
 
