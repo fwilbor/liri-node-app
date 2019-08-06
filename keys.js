800c107f3d7682efcc4b009ea241ecc653183895
fwilbor <franklin.cfg@gmail.com>
2019-08-06 11:15:45 -0400

    update song function to take song name in command line with argv

--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
 console.log("This is Working")
 
-exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+// exports.spotify = {
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+// };
+
+var spotifyKeys = {
+    id: '70b41a5fbe9443ab82ba13867d733702',
+    secret: 'd74dd84e408148c59a5ee2983c707e9b'
 };
+
+module.exports.spotifyKeys = spotifyKeys;
--- liri.js
+++ liri.js
@@ -3,23 +3,62 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // var Spotify = require('node-spotify-api');
-var Spotify = require('spotify-web-api-js');
+var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify();
+
+var spotify = new Spotify({
+    id: '70b41a5fbe9443ab82ba13867d733702',
+    secret: 'd74dd84e408148c59a5ee2983c707e9b'
+});
 
 
 console.log(spotify);
 
 
-//Take in Command Line Argument of Artist Name for Spotify API
-var artist = process.argv[2];
 
-spotify.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
-    .then(function (data) {
-        console.log('Artist information', data);
-    }, function (err) {
-        console.error(err);
+
+//Take in Command Line Argument for "Song Name" for Spotify API
+
+function song() {
+    var commandLine = process.argv;
+    var SongName = "";
+    for (var i = 2; i < commandLine.length; i++) {
+        if (i > 2 && i < commandLine.length) {
+            SongName = SongName + "+" + commandLine[i];
+        }
+        else {
+            SongName += commandLine[i];
+        }
+    }
+
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify({
+        id: '70b41a5fbe9443ab82ba13867d733702',
+        secret: 'd74dd84e408148c59a5ee2983c707e9b'
     });
+
+
+    spotify.search({ type: 'track', query: SongName, limit: 10 }, function (err, data) {
+        if (err) {
+            SongName = "";
+            console.log("Artist: " + songData.artists[0].name);
+            console.log("Song Title: " + songData.name);
+            console.log("Preview Track: " + songData.preview_url);
+            console.log("Album: " + songData.album.name);
+            song();
+        }
+
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            var songData = data.tracks.items[i];
+            console.log("Artist: " + songData.artists[0].name);
+            console.log("Song Title: " + songData.name);
+            console.log("Preview Track: " + songData.preview_url);
+            console.log("Album: " + songData.album.name);
+        }
+    });
+}
+
+song();
 
 
 
