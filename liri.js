ef92c55b8a87b9192a708a578547af63e9d02f28
fwilbor <franklin.cfg@gmail.com>
2019-08-05 22:22:38 -0400

    added Spotify-Node package to make calls to spotify-api using node

--- liri.js
+++ liri.js
@@ -2,55 +2,25 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-
 // var Spotify = require('node-spotify-api');
 var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
+
+var spotify = new Spotify();
+
 
 console.log(spotify);
 
 
-// Grab the axios package...
-var axios = require("axios");
-
 //Take in Command Line Argument of Artist Name for Spotify API
 var artist = process.argv[2];
 
-spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
+spotify.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
     .then(function (data) {
         console.log('Artist information', data);
     }, function (err) {
         console.error(err);
     });
 
-
-// Run the axios.get function...
-// The axios.get function takes in a URL and returns a promise (just like $.ajax)
-axios
-    .get("https://api.spotify.com/v1/artists/{id}" + artist)
-    .then(function (response) {
-        // If the axios was successful...
-        // Then log the body from the site!
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-        }
-        console.log(error.config);
-    });
 
 
 
